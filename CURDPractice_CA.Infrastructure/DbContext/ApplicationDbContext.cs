603642472a5cef05b8194dfe9034e18a7fcc5081
JoKhong <JoKhong@users.noreply.github.com>
2025-11-20 16:29:52 +0800

    Infrastructure

---
+++ CURDPractice_CA.Infrastructure/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public virtual DbSet<Country> Countries { get; set; }
+        public virtual DbSet<Person> Persons { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().ToTable("Countries");
+            modelBuilder.Entity<Person>().ToTable("Persons");
+
+            //Seed countires
+            try
+            {
+                
+                //string countiresJson = File.ReadAllText("countries.json");
+                //List<Country> countries = JsonSerializer.Deserialize<List<Country>>(countiresJson);
+
+                //foreach (Country country in countries)
+                //    modelBuilder.Entity<Country>().HasData(country);
+
+                //string personsJson = File.ReadAllText("persons.json");
+                //List<Person> persons = JsonSerializer.Deserialize<List<Person>>(countiresJson);
+
+                //foreach (Person peson in persons)
+                //    modelBuilder.Entity<Country>().HasData(peson);
+            }
+            catch (Exception ex) { }
+
+            //Fluent API
+            try
+            {
+                modelBuilder.Entity<Person>().Property(temp => temp.TIN)
+                  .HasColumnName("TaxIdentificationNumber")
+                  .HasColumnType("varchar(8)");
+
+                //modelBuilder.Entity<Person>()
+                //  .HasIndex(temp => temp.TIN).IsUnique();
+
+                modelBuilder.Entity<Person>()
+                    .ToTable( Temp => Temp.HasCheckConstraint("CHK_TIN", "len([TaxIdentificationNumber]) = 8"));
+            }
+            catch (Exception ex) { }
+
+        }
+
+        public List<Person> sp_GetAllPersons()
+        {
+            return Persons.FromSqlRaw("EXECUTE [dbo].[GetAllPersons]").ToList();
+        }
+
+        public int sp_InsertPerson(Person person)
+        {
+            SqlParameter[] parameters = new SqlParameter[] {
+        new SqlParameter("@PersonId", person.PersonId),
+        new SqlParameter("@PersonName", person.PersonName),
+        new SqlParameter("@Email", person.Email),
+        new SqlParameter("@DateOfBirth", person.DateOfBirth),
+        new SqlParameter("@Gender", person.Gender),
+        new SqlParameter("@CountryId", person.CountryId),
+        new SqlParameter("@Address", person.Address),
+        new SqlParameter("@ReceiveNewsLetters", person.ReceiveNewsLetters)
+      };
+
+            return Database.ExecuteSqlRaw("EXECUTE [dbo].[InsertPerson] @PersonId, @PersonName, @Email, @DateOfBirth, @Gender, @CountryId, @Address, @ReceiveNewsLetters", parameters);
+        }
+
+    }
+}
---
+++ CURDPractice_CA.Infrastructure/Repositories/PersonsRepositories.cs
@@ -0,0 +1,94 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using RepositoryContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class PersonsRepositories : IPersonsRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly ILogger<PersonsRepositories> _logger;
+
+        public PersonsRepositories(ApplicationDbContext db, ILogger<PersonsRepositories> logger)
+        {
+            _db = db;
+            _logger = logger;
+        }
+
+        public async Task<Person> AddPerson(Person person)
+        {
+            _logger.LogInformation("AddPerson of PersonsRepositories");
+
+            _db.Persons.Add(person);
+            await _db.SaveChangesAsync();
+
+            return person;
+        }
+
+        public async Task<List<Person>> GetAllPersons()
+        {
+            _logger.LogInformation("GetAllPersons of PersonsRepositories");
+            return await _db.Persons.Include("Country").ToListAsync();
+        }
+
+        public async Task<Person?> GetPersonById(Guid personId)
+        {
+            _logger.LogInformation("GetPersonById of PersonsRepositories");
+
+            return await _db.Persons.Include("Country")
+                .FirstOrDefaultAsync(temp => temp.PersonId == personId);
+
+        }
+
+        public async Task<List<Person>?> GetFilteredPersons(Expression<Func<Person, bool>> predicate)
+        {
+            _logger.LogInformation("GetFilteredPersons of PersonsRepositories");
+
+            return await _db.Persons.Include("Country")
+                .Where(predicate)
+                .ToListAsync();
+        }
+
+        public async Task<Person> UpdatePerson(Person person)
+        {
+            _logger.LogInformation("UpdatePerson of PersonsRepositories");
+
+            Person? mathcingPerson = await _db.Persons.Include("Country").FirstOrDefaultAsync(temp => temp.PersonId == person.PersonId);
+            
+            if(mathcingPerson == null)
+                return person;
+
+            mathcingPerson.PersonName = person.PersonName;
+            mathcingPerson.Email = person.Email;
+            mathcingPerson.DateOfBirth = person.DateOfBirth;
+            mathcingPerson.Gender = person.Gender;
+            mathcingPerson.Country = person.Country;
+            mathcingPerson.CountryId = person.CountryId;
+            mathcingPerson.Address = person.Address;
+            mathcingPerson.ReceiveNewsLetters = person.ReceiveNewsLetters;
+            mathcingPerson.TIN = person.TIN;
+
+            await _db.SaveChangesAsync();
+
+            return mathcingPerson;
+
+        }
+
+        public async Task<bool> DeletePersonsById(Guid personId)
+        {
+            _logger.LogInformation("DeletePersonsById of PersonsRepositories");
+
+            _db.Persons.RemoveRange( _db.Persons.Where( temp => temp.PersonId == personId) );
+            int rowsDeleted = await _db.SaveChangesAsync();
+
+            return rowsDeleted > 0;
+        }
+    }
+}
