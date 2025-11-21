27b34989df82978e1dce060c097dc5a72243f52d
JoKhong <JoKhong@users.noreply.github.com>
2025-11-21 10:51:01 +0800

    RegisterDTO and Register Page

---
+++ CURDPractice_CA.Core/DTO/RegisterDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CURDPractice_CA.Core.DTO
+{
+    public class RegisterDto
+    {
+        [Required(ErrorMessage = "Name can't be blank")]
+        public string PersonName;
+
+        [Required(ErrorMessage = "Email can't be blank")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Email value should be a valid email")]
+        public string Email;
+
+        [Required(ErrorMessage = "Phone number can't be blank")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Phone number should only contain numbers")]
+        public string PhoneNumber;
+
+        [Required(ErrorMessage = "Password can't be blank")]
+        [DataType(DataType.Password)]
+        public string Password;
+
+        [Required(ErrorMessage = "Confirm Password can't be blank")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword;
+    }
+}
---
+++ CURDPractice_CA.UI/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using CURD_Practice.Controllers;
+using CURDPractice_CA.Core.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CURDPractice_CA.UI.Controllers
+{
+    [Route("[controller]")]
+    public class AccountController : Controller
+    {
+        [Route("[action]")]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        [HttpPost]
+        public IActionResult Register(RegisterDto registerDto)
+        {
+            if(ModelState.IsValid == false)
+                return View(registerDto);
+
+            //TODO: Save user Data Into DB
+
+            return RedirectToAction( nameof(PersonsController.Index), "Persons");
+        }
+    }
+}
