398ea35a50b08b062a88f774d582d2da122b4f40
JoKhong <JoKhong@users.noreply.github.com>
2025-11-21 12:13:33 +0800

    DBTables

--- CURDPractice_CA.UI/Controllers/AccountController.cs
+++ CURDPractice_CA.UI/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 ﻿using CURD_Practice.Controllers;
+using CURDPractice_CA.Core.Domain.IdentityEntities;
 using CURDPractice_CA.Core.DTO;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CURDPractice_CA.UI.Controllers
@@ -7,23 +9,55 @@ namespace CURDPractice_CA.UI.Controllers
     [Route("[controller]")]
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [Route("[action]")]
         [HttpGet]
-        public IActionResult Register()
+        public async Task<IActionResult> Register()
         {
             return View();
         }
 
         [Route("[action]")]
         [HttpPost]
-        public IActionResult Register(RegisterDto registerDto)
+        public async Task<IActionResult> Register(RegisterDto registerDto)
         {
             if(ModelState.IsValid == false)
+            {
+                ViewBag.Errors = ModelState.Values.SelectMany(x => x.Errors).Select( y => y.ErrorMessage);
                 return View(registerDto);
+            }
 
             //TODO: Save user Data Into DB
+            ApplicationUser user = new ApplicationUser()
+            {
+                Email = registerDto.Email,
+                PhoneNumber = registerDto.PhoneNumber,
+                UserName = registerDto.Email,
+                PersonName = registerDto.PersonName
+            };
 
-            return RedirectToAction( nameof(PersonsController.Index), "Persons");
+            IdentityResult result =  await _userManager.CreateAsync(user);
+
+            if(result.Succeeded)
+            {
+                return RedirectToAction(nameof(PersonsController.Index), "Persons");
+            }
+            else
+            {
+                foreach(IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("Register", error.Description);
+                }
+
+                return View(registerDto);
+            }
+
         }
     }
 }
